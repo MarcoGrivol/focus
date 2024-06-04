7e0a294b37d77fa757254ded78ac53e44e8266d0
MarcoDesktop <mbgrivol@gmail.com>
2024-06-03 22:54:39 -0300

    sample parsing functionality working

--- crawler.py
+++ crawler.py
@@ -3,37 +3,97 @@ import re
 import logging
 
 from os import path
+from collections import namedtuple
+from typing import AnyStr, LiteralString, List, Tuple
 
 QUESTIONS_HEADING = 'Anki Questions'
-
+# #[...] [...]ANYTHING[spaces]\n
+# Group: (1)=hashtags, (2)=heading text
+RE_HEADING = re.compile(r'(#+?) +(.+?) *\n')
 # Target: #anki/DECK/TAG[/SUB_TAGS]
 # Group: DECK e TAG[/SUB_TAGS], used to determine the Anki deck and Note tags
 RE_ANKI_TAG = re.compile(r'(#anki)/(\S+?)/(\S.+)')
-# RE_QUESTIONS_HEAD = re.compile(r'(#)+? +?' + QUESTIONS_HEADING + r' *?\n')
-# Target: 1. QUESTION TEXT
+# Target: NUMBER. QUESTION TEXT
 # Group: returns the text of the question
-RE_QUESTIONS_BODY = re.compile(r'\d+\. (.+)')
+RE_CARD_BODY = re.compile(r'(?:^|\n\s*)\d+\. +(.+)')  # re.compile(r'(?<=\n)\s*\d+\. +(.+)')
+# Targe: QUESTION .|? [[text[#op]|ans]]
+# Groups: (1)=question, (2)=link
+RE_CARD_ENTRY = re.compile(r'(.+?[.?])\s*(?:\[\[(.+\|ans)]])*')
 # Target: [[ANY TEXT BETWEEN TWO BRACKETS]]
 # Group: text in between, may contain the optional #heading link or |link renaming tokens
-RE_LINKS = re.compile(r'\[\[(.*?)]]')
-
-# [[link#header|ans]] groups: link, header, ans
-# RE_LINKS = re.compile(r'\[\[([a-zA-Z0-9 ]+)#?([a-zA-Z0-9 ]*?)\|?(ans)*?]]')
-
-logger = logging.getLogger(__name__)
+RE_LINKS = re.compile(r'\[\[(.+?)]]')
 
 
-# Target: #[#...#] [space...space]HEADING[space..space]\n
-# Group: hashtags, used to figure out the heading level
-def regex_heading(heading):
-    return re.compile(r'(#)+? +?' + heading + r' *?\n')
+ObsidianLink = namedtuple('ObsidianLink', ['path', 'name', 'heading', 'alias'])
 
 
-class ObsidianNote:
+def re_heading(heading):
+    return re.compile(r'(#+?) +' + heading + r' *\n')
+
+
+def parse_link(link: str) -> ObsidianLink:
+    # file[#heading][|rename]
+    s = link.split('|')  # file[#heading], [|rename]
+    alias = s[1] if len(s) == 2 else ''
+    s = s[0].split('#')  # file, [#heading]
+    heading = s[1] if len(s) == 2 else ''
+    name = s[0]
+    return ObsidianLink(f'{s[0]}.md', s[0], heading, alias)
+
+
+def find_heading(data: str, heading: str, exit_on_first=False) -> str:
+    """
+    Find all the text encapsulating a heading, not the heading itself.
+    :param data: file contents.
+    :param heading: the name of the heading, excluding the '#' and any leading or trailing whitespace.
+    :param exit_on_first: if True, stops after finding the next heading.
+    """
+    if re_heading(heading).search(data) is None:
+        return ''
+
+    heads = RE_HEADING.split(data)
+
+    target_heading = 0
+    text = ''
+
+    for i, s in enumerate(heads):
+        if s == '#' * len(s):  # only hashtags
+            if heads[i + 1] == heading:
+                target_heading = len(s)
+            elif target_heading != 0:  # already found the target heading
+                if exit_on_first:
+                    break
+                elif len(s) <= target_heading:  # found a higher level heading, stop
+                    break
+            continue
+
+        if target_heading == 0:
+            continue  # text before heading
+
+        text += s
+    return text.removeprefix(heading).strip('---').strip()
+
+
+def navigate(vault: str, link: ObsidianLink, exit_on_first=False):
+    filepath = path.join(vault, link.path)
+    if not path.isfile(filepath):
+        return ''
+
+    with open(filepath, 'r', encoding='utf-8') as f:
+        data = f.read()
+
+    if link.heading:
+        return find_heading(data, link.heading, exit_on_first)
+
+    return data.strip()
+
+
+
+class ObsidianFile:
     def __init__(self, vault, filepath):
         self.vault = vault
         self.filepath = filepath
-        self.link = filepath.removeprefix(vault).removesuffix('.md')
+        self.name = filepath.removeprefix(vault).removesuffix('.md')
 
         with open(filepath, 'r', encoding='utf-8') as f:
             self.data = f.read()
@@ -42,134 +102,84 @@ class ObsidianNote:
         if anki_tag is None:
             self.is_anki = False
             self.deck = self.tag = None
-            self.fields = []
+            self.cards = []
         else:
             self.is_anki = True
             self.deck = anki_tag.group(1)
             self.tag = anki_tag.group(2)
-            self.questions = self._read_questions()
-            self.answers = self._read_answers()
+            self.cards = self._parse()
 
-    def _read_questions(self):
-        questions = RE_QUESTIONS_BODY.findall(
-            self._find_heading(QUESTIONS_HEADING)
-        )
+    def _parse(self) -> List[ Tuple[str, str] ]:
+        text = find_heading(self.data, QUESTIONS_HEADING)
 
+        cards = list()
 
-    def _find_heading(self, heading, exit_on_first=False):
-        """
-        Find all the text encapsulating a heading.
-        :param heading: the name of the heading, excluding the '#' and any leading or trailing whitespace.
-        :param exit_on_first: if True, stops after finding the next heading.
-        :return: the encapsulated heading text, excluding the heading itself and any '---' or '\n' trailing chars.
-        """
-        text_before, text = self.data.split(f'# {heading}', maxsplit=1)
+        for card in RE_CARD_BODY.findall(text):
+            questions, answers = list(), list()
 
-        heading_level = 1
-        for c in text_before[::-1]:
-            if c == '#':
-                heading_level += 1
-            else:
-                break
+            for i, card_entry in enumerate(RE_CARD_ENTRY.findall(card)):
+                q, ans = self._parse_card_entry(card_entry)
+                if q == '' or ans == '':
+                    break
+                questions.append(q)
+                answers.append(ans)
 
-        heading_text = ''
+            # if '' in questions or '' in answers:
+            #     cards.append(zip())
+            # else:
+            cards.append(zip(questions, answers))
 
-        for line in text.split('\n'):
-            h = re.match(r'(#+?) ', line)
-            if h is not None:
-                if exit_on_first:
-                    break  # first heading found
-                elif len(h.group(1)) <= heading_level:
-                    break  # found a higher level heading
-                # child heading, proceed
-            heading_text += line + '\n'
+        return cards
 
-        return heading_text.strip('\n').strip('---')
+    def _parse_card_entry(self, card_entry) -> Tuple[str, str]:
+        question, answer = card_entry
 
+        if answer:
+            return question, self._goto(parse_link(answer))
 
-def load_note(vault, file):
-    with open(file, 'r', encoding='utf-8') as reader:
-        data = reader.read()
+        links = RE_LINKS.findall(question)
+        if len(links) != 1:
+            return '', ''
 
-    ob_file = path.basename(file).removesuffix('.md')
+        return question, self._goto(parse_link(links[0]))
 
-    anki_tag = RE_ANKI_FIELDS.search(data)
-    deck = anki_tag.group(1)
-    tag = anki_tag.group(2)
-
-    q_text = find_heading(data, QUESTIONS_TOKEN, exit_on_first=True)
-    q_text = list(filter(lambda x: x is not None and x != '---', q_text.split('\n')))
-    q_text = [q.split('. ', maxsplit=1)[1] for q in q_text]
-
-    for i, q in enumerate(q_text):
-        print(f'({i}): {q}')
-
-    notes = list()
-
-    for i, q in enumerate(q_text[:1]):
-        ans = find_answer(vault, ob_file, data, q)
-
-
-def find_answer(vault, ob_file, data, question):
-    links = RE_LINKS.findall(question)
-
-    if len(links) == 0:
-        logger.error('no links found')
-        return ''
-
-    if len(links) == 1:
-        parsed = parse_link(links[0])
-        return read_link(vault, ob_file, data, parsed)
-
-    parsed = [parse_link(x) for x in links]
-    ans_link = list(filter(lambda x: x[2] == 'ans', parsed))
-
-    if len(ans_link) == 1:
-        return read_link(vault, ob_file, data, ans_link[0])
-
-    logger.error(f'{links} contains incorrect number of "ans" link tags, expected 1 but got {len(ans_link)}')
-    return ''
-
-
-def parse_link(link):
-    # file[#heading][|rename]
-    s = link.split('|')  # file[#heading], [|rename]
-    name = s[1] if len(s) == 2 else ''
-    s = s[0].split('#')  # file, [#heading]
-    heading = s[1] if len(s) == 2 else ''
-    return s[0], heading, name
-
-
-def read_link(vault, ob_file, data, link, exit_on_first=False):
-    file, heading, _ = link
-
-    if file != ob_file:
-        with open(path.join(vault, f'{file}.md'), 'r', encoding='utf-8') as reader:
-            data = reader.read()
-
-    if heading:
-        return find_heading(data, heading, exit_on_first)
-    return data
+    def _goto(self, link: ObsidianLink) -> str:
+        if link.name == self.name:
+            return find_heading(self.data, link.heading)
+        return navigate(self.vault, link)
 
 
 def _main():
     db = 'C:\\mine\\Journey'
 
-    anki_files = list()
+    vault_files = list()
+    ob_files = list()
 
     for root, dirs, files in os.walk(db):
         for f_name in files:
-            f_path = os.path.join(root, f_name)
 
-            if is_anki(f_path):
-                anki_files.append(f_path)
+            f_path = os.path.join(root, f_name)
+            if not f_name.endswith('.md'):
+                continue
+
+            vault_files.append(f_path)
+
+            obf = ObsidianFile(db, f_path)
+            if obf.is_anki:
+                ob_files.append(obf)
 
     print('ANKI FILES:')
-    for i, f in enumerate(anki_files):
-        print(f'\t{i}: {f}')
-
-        load_note(db, f)
-
+    for i, obf in enumerate(ob_files):
+        if len(obf.cards) == 0:
+            print(f'{i}: {obf.filepath}\n\t')
+        else:
+            print(f'{i}: {obf.filepath}:')
+            for j, card in enumerate(obf.cards):
+                print(f' card {j}')
+                for q, ans in card:
+                    ans = ans.replace('\n', '\\n')
+                    print(f'\t\tQ:   {q}')
+                    print(f'\t\tAns: {ans}')
 
 
 if __name__ == '__main__':
